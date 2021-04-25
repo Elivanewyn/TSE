0b08c28d82fd7006e85257320188587807a8b7fd
qsrnova <80409878+qsrnova@users.noreply.github.com>
2021-04-25 14:03:14 +0100

    1st Boss (Still In Progress)

---
+++ Assets/Scripts/angelAttack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angelAttack : MonoBehaviour
+{
+    public int attackDamage = 2;
+    public int enrageAttackDamage = 4;
+
+
+}
